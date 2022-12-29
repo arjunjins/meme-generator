52ed5a85e4ed54c9b710c907474d267934977c4a
Arjun J <arjunjins2001@gmail.com>
2022-12-30 00:23:55 +0800

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="logo">
+        <img src="../../../Troll Face.png" alt="Troll face" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
