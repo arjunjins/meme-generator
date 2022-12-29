9486ef384f613dea7104d7e62960d1ef58231cd0
Arjun J <arjunjins2001@gmail.com>
2022-12-30 00:29:21 +0800

    initial header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,9 @@ const Header = () => {
     <div className="Header">
       <div className="logo">
         <img src="../../../Troll Face.png" alt="Troll face" />
+        <h1>Meme Generator</h1>
       </div>
+      <h3>React Course Project - 3</h3>
     </div>
   );
 };
