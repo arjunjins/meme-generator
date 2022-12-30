43a252db5e3e6007b86eb156d0691fa008a264ab
Arjun J <arjunjins2001@gmail.com>
2022-12-30 11:12:49 +0800

    test api

--- src/components/Meme/Meme.jsx
+++ src/components/Meme/Meme.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Meme.css";
+import memesData from "./memesData";
 const Meme = () => {
+  function handleClick() {
+    let randomNum = Math.floor(Math.random() * 100);
+    let img = memesData.data.memes[randomNum].url;
+    let name = memesData.data.memes[randomNum].name;
+    console.log("Image ID : " + img);
+    console.log("Name : " + name);
+  }
   return (
     <main className="meme">
       <div>
         <div className="form">
           <input type="text" placeholder="Top Text" className="inp" />
           <input type="text" placeholder="Bottom Text" className="inp" />
-          <button type="submit" className="buttn">
+          <button type="submit" className="buttn" onClick={handleClick}>
             Get a new meme image 🖼
           </button>
         </div>
